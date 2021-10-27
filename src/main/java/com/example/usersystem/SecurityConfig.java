9bacc94fe20f5eb375251f1ab4298424557f947c
Roozbeh <mmroozbeh2000@gmail.com>
2021-10-27 18:07:34 +0200

    Cancel knappar färg

--- src/main/java/com/example/usersystem/SecurityConfig.java
+++ src/main/java/com/example/usersystem/SecurityConfig.java
@@ -20,7 +20,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .and()
-                .logout();
+                .logout()
+                ;
+
 
 
     }
